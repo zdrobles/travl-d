fb57f30b57e54ac0366f8950a32a34985e33384e
Zach Robles <45700544+zdrobles@users.noreply.github.com>
2022-11-14 14:45:31 -0500

    initial commit

---
+++ backend/src/main/java/com/zach/worldcities/WorldCitiesApplication.java
@@ -0,0 +1,13 @@
+package com.zach.worldcities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldCitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldCitiesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zach/worldcities/WorldCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zach.worldcities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldCitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
